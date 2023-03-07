fa3713cb8a42f4757bc1f4d5732fb15a13b4715e
jakubZimnyGit <jakub.zimny.gdansk@gmail.com>
2023-03-07 23:02:51 +0100

    pseudokod update

---
+++ ZadaniaZTematuWprowadzenie/cw4 π4/cw4 π4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw4_π4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ciagLiczb = { -1, 1, 2, 3, 4, 8, 10, 21 };
+        }
+        static bool IsXInSortedArray(int[] array, int x) 
+        {
+
+        }
+    }
+}
